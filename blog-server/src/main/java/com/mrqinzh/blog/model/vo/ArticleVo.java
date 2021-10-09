d61689700d328fc0056440ce47f24c94421f0670
qin zhihong <1552589784@qq.com>
2021-10-09 15:57:08 +0800

    '引入mybatis-plus'

---
+++ blog-server/src/main/java/com/mrqinzh/blog/model/vo/ArticleVo.java
@@ -0,0 +1,13 @@
+package com.mrqinzh.blog.model.vo;
+
+/**
+ *  company 中科大业
+ *  FileName ArticleVo
+ *  Package com.mrqinzh.blog.model.vo
+ *  Description 
+ *  author BIM7
+ *  create 2021-10-09 15:50
+ *  version V1.0
+ */
+public class ArticleVo {
+}

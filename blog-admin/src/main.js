319125edd9a8f0943b6af0481eb40e0e1c685388
qin zhihong <1552589784@qq.com>
2021-10-14 15:18:49 +0800

    '1'

--- blog-admin/src/main.js
+++ blog-admin/src/main.js
@@ -4,13 +4,13 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-import animated from 'animate.css'
+import animated from 'animate.css' // animate.css
+Vue.use(animated)
+
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 import Antd from 'ant-design-vue'// 引入Ant Design Vue组件
 Vue.use(Antd) //挂载到vue中
-Vue.use(animated)
 
 import '@/styles/index.scss' // global css
 
@@ -21,23 +21,7 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-// if (process.env.NODE_ENV === 'production') {
-//   const { mockXHR } = require('../mock')
-//   mockXHR()
-// }
-
-// set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 

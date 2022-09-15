ee3e897b4858bee9e14b04ab8626a27f403204af
秦志宏 <1552589784@qq.com>
2022-09-15 22:20:31 +0800

    拆分完成

--- blog-server/blog-api/src/main/java/com/mrqinzh/api/BlogApiApplication.java
+++ blog-server/blog-api/src/main/java/com/mrqinzh/api/BlogApiApplication.java
@@ -1,14 +1,24 @@
 package com.mrqinzh.api;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@MapperScan("com.mrqinzh.blog.mapper")
+@SpringBootApplication(scanBasePackages = "com.mrqinzh")
+@MapperScan("com.mrqinzh.server.mapper")
 public class BlogApiApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(BlogApiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(BlogApiApplication.class, args);
+        try {
+            SpringApplication.run(BlogApiApplication.class, args);
+            logger.info("============== BlogApiApplication start success ==============");
+        } catch (Exception e) {
+            logger.error("============== BlogApiApplication start failed ==============", e);
+            throw e;
+        }
     }
 }

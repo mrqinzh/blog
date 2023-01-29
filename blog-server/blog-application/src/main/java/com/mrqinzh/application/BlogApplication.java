ca509c89cf2dded7edaf2a8040aaa36b566e379e
秦志宏 <1552589784@qq.com>
2023-01-29 20:31:36 +0800

    发布

--- blog-server/blog-application/src/main/java/com/mrqinzh/application/BlogApplication.java
+++ blog-server/blog-application/src/main/java/com/mrqinzh/application/BlogApplication.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
-@SpringBootApplication(scanBasePackages = "com.mrqinzh", exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(scanBasePackages = "com.mrqinzh")
 public class BlogApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(BlogApplication.class);
